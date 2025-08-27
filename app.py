9dd189e927ce0d10cc9ffbd5df7e0473fca011d9
channi84 <channi.kr@gmail.com>
2025-08-27 17:01:30 +0900

    할일 추가하기

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
+todo_list = ["파이썬 공부하기", "운동하기"]
+
 @app.route('/')
 def home():
-    return redirect(url_for('todo_list'))
+    return redirect(url_for('todo_list_page'))
 
-# 새로운 라우트 추가
 @app.route('/todo')
-def todo_list():
-    return render_template('todo.html')
+def todo_list_page():
+    return render_template('todo.html', todo_list=todo_list)
+
+@app.route('/add', methods=['POST'])
+def add_todo():
+    todo_item = request.form.get('todo_item')
+    if todo_item:
+        todo_list.append(todo_item)
+    return redirect(url_for('todo_list_page'))
 
 if __name__ == '__main__':
     app.run(debug=True)
